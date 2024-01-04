1f28d0efba659ddd0cb8433c58f8e4478925efaf
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-01-04 15:56:37 -0800

    Update divPage8.php

--- buyer/divPage8.php
+++ buyer/divPage8.php
@@ -13,13 +13,20 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <!doctype html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
-<link rel="stylesheet" type="text/css" href="files/css/flags.css" />
+<link rel="stylesheet" type="text/css" href="../layout/admin-theme/css/custom.min.css?1" />
+<link rel="stylesheet" type="text/css" href="../layout/admin-theme/css/custom.css?1" />
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
-<script type="text/javascript" src="files/js/jquery.js?1"></script>
-<script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
-<script type="text/javascript" src="files/js/sorttable.js"></script>
-<script type="text/javascript" src="files/js/table-head.js?3334"></script>
+
+
+<script type="text/javascript" src="files/js/code.jquery.com/jquery-3.7.0.min.js"></script>
+<script type="text/javascript" src="../layout/admin-theme/assets/bootbox.min.js?1"></script>
+
+
+
+
+
+<script type="text/javascript" src="../layout/admin-theme/assets/bootbox.min.js?1"></script>
 <script type="text/javascript" src="files/js/bootbox.min.js"></script>
 <script type="text/javascript" src="files/js/clipboard.min.js"></script>
 
