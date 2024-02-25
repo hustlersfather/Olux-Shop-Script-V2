cd483fee6d6af228531e4bd314c843d6befd1b96
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-02-25 10:58:09 -0800

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -5,61 +5,34 @@ include "includes/config.php";
 date_default_timezone_set('UTC');
 
 if(isset($_SESSION['sname']) && isset($_SESSION['spass'])){
-    header("location: /buyer/index.php");
+    header("location: index.html");
     exit();
 }
 
-if (isset($_POST['username'],$_POST['email'],$_POST['password_signup'],$_POST['password_signup2'])) {
-    $uname = mysqli_real_escape_string($dbcon, strip_tags($_POST['username']));
-    $email = mysqli_real_escape_string($dbcon, strip_tags($_POST['email']));
-    $pass1 = mysqli_real_escape_string($dbcon, strip_tags($_POST['password_signup']));
-    $pass2 = mysqli_real_escape_string($dbcon, strip_tags($_POST['password_signup2']));
-    $ip    = getenv("REMOTE_ADDR");
-    $rdate = date("y-m-d");
-    $lvisi = date('y-m-d');
+if (isset($_POST['user'], $_POST['pass'])) {
+    $username = mysqli_real_escape_string($dbcon, strip_tags($_POST['user']));
+    $password = mysqli_real_escape_string($dbcon, strip_tags($_POST['pass']));
+    $lvisi = date('Y-m-d');
 
-    $passstrlen = strlen($pass1);
+    $finder = mysqli_query($dbcon, "SELECT * FROM users WHERE username='".strtolower($username)."'") or die("mysqli error");
 
-    $result = mysqli_query($dbcon, "SELECT * FROM users WHERE username='".$uname."'");
-    $userexist = mysqli_num_rows($result);
-
-    $result = mysqli_query($dbcon, "SELECT * FROM users WHERE email='".$email."'");
-    $emailexist = mysqli_num_rows($result);
-
-    if(empty($uname) || empty($email) || empty($pass1) || empty($pass2)){
-        $errorbox = "<div class='alert alert-dismissible alert-info'><button type='button' class='close' data-dismiss='alert'>×</button><p>Please check all entries</p></div>";
-        echo '{"state":"0","errorbox":"'.$errorbox.'","url":""}';
-    } elseif(strlen($uname) > 16){
-        $errorbox = "<div class='alert alert-dismissible alert-info'><button type='button' class='close' data-dismiss='alert'>×</button><p>Username must be less than 16 chars.</p></div>";
-        echo '{"state":"0","errorbox":"'.$errorbox.'","url":""}';
-    } elseif($userexist == 1 || $uname == "NONE" || $uname == "NULL" || $uname == "SYSTEM" || $uname == "none" || $uname == "system"){
-        header('location:signup.html?error=userexist');
-        exit;
-    } elseif($emailexist == 1){
-        header('location:signup.html?error=emailexist');
-        exit;
-    } elseif($pass1 != $pass2){
-        header('location:signup.html?error=passnotmatch');
-        exit;
-    } elseif($passstrlen < 6 || $passstrlen > 16){
-        header('location:signup.html?error=passlength');
-        exit;
-    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $errorbox = "<div class='alert alert-dismissible alert-info'><button type='button' class='close' data-dismiss='alert'>×</button><p>Invalid email!</p></div>";
-        echo '{"state":"0","errorbox":"'.$errorbox.'","url":""}';
+    if(mysqli_num_rows($finder) != 0){
+        $row = mysqli_fetch_assoc($finder);
+        if(strtolower($username) == strtolower($row['username']) && $password == $row['password']){
+            $_SESSION['sname'] = $username;
+            $_SESSION['spass'] = $password;
+            header('location:index.html');
+            exit();
+        } else {
+            header('location:login.html?error=true');
+            exit();
+        }
     } else {
-        $password = $pass1;
-
-        $insert = mysqli_query($dbcon, "INSERT INTO users
-        (username,password,email,balance,ipurchassed,ip,lastlogin,datereg,resseller,img,testemail,resetpin)
-        VALUES
-        ('$uname','$password','$email','0','0','$ip','$lvisi','$rdate','0','','$email',0)") or die(mysqli_error($dbcon));
-
-        header('location:login.html?success=register');
-        exit;
+        header('location:login.html?error=true');
+        exit();
     }
 } else {
     header('location:index.html');
-    exit;
+    exit();
 }
 ?>
