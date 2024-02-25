8257c336b9b1be8a39e84595801311c611264197
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-02-25 06:56:22 -0800

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -1,42 +1,47 @@
 <?php
-session_start();
-ob_start();
+  ob_start();
+  session_start();
+  include "includes/config.php";
+  include 'encrypt.php';
+  date_default_timezone_set('UTC');
+  
 
-date_default_timezone_set('UTC');
-
-include "includes/config.php";
-include 'encrypt.php';
-
-if(isset($_SESSION['sname']) && isset($_SESSION['spass'])) {
-    header("location: index.html");
+  if(isset($_SESSION['sname']) and isset($_SESSION['spass'])){
+   header("location: /buyer/index.php");
+   exit();
+  }
+  if (isset($_POST['user'],$_POST['pass'])) {
+    # code...
+  } else{
+    header('location:/buyer/index.php');
     exit();
-}
-
-if (isset($_POST['user'], $_POST['pass'])) {
-    $username = mysqli_real_escape_string($dbcon, $_POST['user']);
-    $password = mysqli_real_escape_string($dbcon, $_POST['pass']);
-    $userpass = dec_enc('encrypt', $password);
-    $lvisi = date('Y-m-d');
-
-    $finder = mysqli_query($dbcon, "SELECT * FROM users WHERE username='".strtolower($username)."' AND password='".$userpass."'") or die("mysqli error");
-
-    if(mysqli_num_rows($finder) != 0){
-        $row = mysqli_fetch_assoc($finder);
-        if(strtolower($username) == strtolower($row['username']) && $userpass == $row['password']) {
-            $_SESSION['sname'] = $username;
-            $_SESSION['spass'] = $userpass;
-            header('location:index.html');
-            exit();
-        } else {
-            header('location:login.html?error=true');
-            exit();
-        }
-    } else {
-        header('location:login.html?error=true');
-        exit();
+  }
+  $username = mysqli_real_escape_string($dbcon, strip_tags($_POST['user']));
+  $passnotc = mysqli_real_escape_string($dbcon, strip_tags($_POST['pass']));
+  $userpass = dec_enc('encrypt',$passnotc);
+  $lvisi = date('Y-m-d');
+ $finder = mysqli_query($dbcon, "SELECT * FROM users WHERE username='".strtolower($username)."' AND password='".$userpass."'") or die("mysqli error");
+  if(mysqli_num_rows($finder) != 0){
+    $row = mysqli_fetch_assoc($finder);
+    if(strtolower($username) == strtolower($row['username']) and $userpass==$row['password']){
+     //$sname = $_SESSION['sname'];
+     $_SESSION['sname'] = $username;
+     $_SESSION['spass'] = $userpass;
+	  //$errorbox = "<div class='alert alert-dismissible alert-success'><button type='button' class='close' data-dismiss='alert'>×</button><p>Login successful. Redirecting …</p></div>";
+      //echo '{"state":"1","errorbox":"'.$errorbox.'","url":"main"}';
+     header('location:buyer/');
+     exit();
+	  }else{
+	  //$errorbox = "<div class='alert alert-dismissible alert-info'><button type='button' class='close' data-dismiss='alert'>×</button><p>Login failed! Please try again! 1</p></div>";
+      //echo '{"state":"0","errorbox":"'.$errorbox.'","url":"0"}';
+      header('location:login?error=true');
+      exit();
     }
-} else {
-    header('location:index.html');
-    exit();
-}
-?>
+  }else{
+	  //$errorbox = "<div class='alert alert-dismissible alert-info'><button type='button' class='close' data-dismiss='alert'>×</button><p>Login failed! Please try again! 2</p></div>";
+      //echo '{"state":"0","errorbox":"'.$errorbox.'","url":"0"}';
+      header('location:login?error=true');
+      exit();
+    }
+  
+  ?>
