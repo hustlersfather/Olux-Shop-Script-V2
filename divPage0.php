85c8cbeda681834ffce00543f04a8a6e5fd747c3
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-02-25 05:27:12 -0800

    Update divPage0.php

--- divPage0.php
+++ divPage0.php
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 <?php
 ob_start();
 session_start();
@@ -27,7 +20,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
     If you have any <b>Question</b> ,<b>Problem</b>, <b>Suggestion</b> or <b>Request</b> Please feel free to <a class="label label-default " href="tickets.html"><span class="glyphicon glyphicon-pencil"></span> Open a Ticket</a><br>
     if you want to report an order , just go to <abbr title="Account - > My Orders or Click here" >My Orders  <span class="glyphicon glyphicon-shopping-cart"></span></abbr> 
     then click on <a class="label label-primary">Report #[Order Id]</a> button<br><br>
-    Our Domains are <b>Felux.shop</b>- Please Save them!
+    Our Domains are <b>Jerux.to</b> || <b>Jerux.xyz</b> || <b>Jerux.shop</b> || <b>Jerux.pw</b> - Please Save them!
 
 </div>
 
@@ -50,12 +43,12 @@ while($r = mysqli_fetch_assoc($qq)){				echo'<a class="list-group-item"><h5 clas
     ';
 	?>
 	<div class="well well-sm">    
-                  <h4><b>Our Support team is here !</b></h4><a class="btn btn-default btn-sm" onclick="pageDiv(9,'Tickets - FeluxShop','tickets.html#open',0); return false;" href="tickets.html#open"><span class="glyphicon glyphicon-pencil"></span> Open a Ticket</a>
-                  <h5><b>Interested in becoming a seller at  Olux Shop ?</b></h5><a class="btn btn-primary btn-xs" href="seller.html" onclick="pageDiv(24,'Become Seller  - FeluxShop','seller.html',0); return false;">Learn more</a>
+                  <h4><b>Our Support team is here !</b></h4><a class="btn btn-default btn-sm" onclick="pageDiv(9,'Tickets - OluxShop','tickets.html#open',0); return false;" href="tickets.html#open"><span class="glyphicon glyphicon-pencil"></span> Open a Ticket</a>
+                  <h5><b>Interested in becoming a seller at  Olux Shop ?</b></h5><a class="btn btn-primary btn-xs" href="seller.html" onclick="pageDiv(24,'Become Seller  - OluxShop','seller.html',0); return false;">Learn more</a>
                   <h5><b>Available Payment Methods </b></h5>
 
-                  <img src="files/img/pmlogo2.png" height="48" width="49" title="PerfectMoney" onclick="pageDiv(11,'Add Balance - FeluxShop','addBalance.html#perfectmoney',0); return false;" href="addBalance.html#perfectmoney" onmouseover="this.style.cursor='pointer'">
-                  <img src="files/img/btclogo.png" height="48" width="49" title="Bitcoin" onclick="pageDiv(11,'Add Balance - FeluxShop','addBalance.html#bitcoin',0); return false;" href="addBalance.html#bitcoin" onmouseover="this.style.cursor='pointer'">
+                  <img src="files/img/pmlogo2.png" height="48" width="49" title="PerfectMoney" onclick="pageDiv(11,'Add Balance - OluxShop','addBalance.html#perfectmoney',0); return false;" href="addBalance.html#perfectmoney" onmouseover="this.style.cursor='pointer'">
+                  <img src="files/img/btclogo.png" height="48" width="49" title="Bitcoin" onclick="pageDiv(11,'Add Balance - OluxShop','addBalance.html#bitcoin',0); return false;" href="addBalance.html#bitcoin" onmouseover="this.style.cursor='pointer'">
                  
       </div>
 	<?php
