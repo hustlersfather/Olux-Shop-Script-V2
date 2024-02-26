a0dfc85d5e08f14d136ba35447da75bc12bf2253
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2024-02-25 18:22:22 -0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,15 @@
 <?php
+// Database credentials
+$db_host = "localhost";
+$db_user = "xbasoxxc_xbasoxxc";
+$db_password = "xbasoxxc_xbasoxxc";
+$db_name = "xbasoxxc_test";
 
+// Attempt to establish a connection to the MySQL database
+$dbcon = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
-$dbcon = mysqli_connect("localhost","xbasoxxc_xbasoxxc","xbasoxxc_xbasoxxc","xbasoxxc_test") or die('enable connection'); 
-
-
-
+// Check if the connection was successful
+if (!$dbcon) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
